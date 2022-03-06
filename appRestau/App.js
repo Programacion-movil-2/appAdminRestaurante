08503cb381152def6d9bbec4f362b9d6c9a50a87
Edgardo Mejia <edmejia272@gmail.com>
2022-03-06 01:55:22 -0600

    Login v1

--- appRestau/App.js
+++ appRestau/App.js
@@ -1,14 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
-import SinginScreen from './screens/SinginScreen';
+import SignInScreen from './screens/SignInScreen/SignInScreen';
 
 export default class App extends React.Component {
   render(){
     return (
 
       <SafeAreaView style={styles.root}>
-        <SinginScreen />
+        <SignInScreen />
       </SafeAreaView>
       
   
