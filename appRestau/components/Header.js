bdfb491663b2440dea01af6b54d3dd906c94c026
Ericka Castellanos <erickacastellanosalv@gmail.com>
2022-03-05 22:25:04 -0600

    Creación de components para las pantallas

---
+++ appRestau/components/Header.js
@@ -0,0 +1,24 @@
+import React from "react";
+import{
+    View,
+    Text
+} from 'react-native';
+import{ FONTS } from "../constants";
+
+const Header = ({containerStyle, title}) => {
+    return (
+        <View
+            style = {{
+                flexDirection: 'row',
+                ...containerStyle
+            }}
+        >
+            {/* Left */}
+
+            {/* Left */}
+
+            {/* Left */}
+
+        </View>
+    )
+}
