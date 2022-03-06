da91219e08f6c7688d40f10769f9d92ed11af99c
HectorOC-e <osoriohector89@gmail.com>
2022-03-06 13:13:07 -0600

    Merge branch 'Ericka' of https://github.com/Programacion-movil-2/AppRestaurante into developer

--- appRestau/screens/Cart/MyCart.js
+++ appRestau/screens/Cart/MyCart.js
@@ -26,6 +26,32 @@ const MyCart = ({ navigation }) => {
                     marginHorizontal: SIZES.padding,
                     marginTop:40
                 }}
+                leftComponent = {
+                    <IconButton
+                        icon = {icons.back}
+                        containerStyle = {{
+                            with: 40,
+                            height: 40,
+                            justifyContent: 'center',
+                            alignItems: 'center',
+                            borderWidth: 1,
+                            borderRadius: SIZES.radius,
+                            borderColor: COLORS.gray2
+                        }}
+                        iconStyle = {{
+                            width: 20,
+                            height: 20,
+                            tintColor: COLORS.gray2
+                        }}
+                        //Con este boton vamos a regresar a los detalles de la comida y categorias
+                        onPress = {() => navigation.goBack()}
+                    />
+                }
+                rightComponent = {
+                    <CartQuantityButton
+                        quantity = {3}
+                    />
+                }
             />
         )
     }
