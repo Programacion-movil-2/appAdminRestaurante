5ba57cd957a271542679bcaa954fcc457736aecb
HectorOC-e <osoriohector89@gmail.com>
2022-03-06 20:49:35 -0600

    Intento de resolucion #8923

--- appRestau/screens/mainCategorias.js
+++ appRestau/screens/mainCategorias.js
@@ -14,14 +14,6 @@ import { icons, images, SIZE, COLORS } from '../constants'
 
 const MainCategorias = () => {
 
-    const initialCurrentLocation = {
-        streetName: "Kuching",
-        gps: {
-            latitude: 1.5496614931250685,
-            longitude: 110.36381866919922
-        }
-    }
-
     const categoryData = [
         {
             id: 1,
@@ -90,10 +82,6 @@ const MainCategorias = () => {
             priceRating: affordable,
             photo: images.burger_restaurant_1,
             duration: "30 - 45 min",
-            location: {
-                latitude: 1.5347282806345879,
-                longitude: 110.35632207358996,
-            },
             courier: {
                 avatar: images.avatar_1,
                 name: "Amy"
@@ -133,10 +121,6 @@ const MainCategorias = () => {
             priceRating: expensive,
             photo: images.pizza_restaurant,
             duration: "15 - 20 min",
-            location: {
-                latitude: 1.556306570595712,
-                longitude: 110.35504616746915,
-            },
             courier: {
                 avatar: images.avatar_2,
                 name: "Jackson"
@@ -184,10 +168,6 @@ const MainCategorias = () => {
             priceRating: expensive,
             photo: images.hot_dog_restaurant,
             duration: "20 - 25 min",
-            location: {
-                latitude: 1.5238753474714375,
-                longitude: 110.34261833833622,
-            },
             courier: {
                 avatar: images.avatar_3,
                 name: "James"
@@ -211,10 +191,6 @@ const MainCategorias = () => {
             priceRating: expensive,
             photo: images.japanese_restaurant,
             duration: "10 - 15 min",
-            location: {
-                latitude: 1.5578068150528928,
-                longitude: 110.35482523764315,
-            },
             courier: {
                 avatar: images.avatar_4,
                 name: "Ahmad"
@@ -238,10 +214,6 @@ const MainCategorias = () => {
             priceRating: affordable,
             photo: images.noodle_shop,
             duration: "15 - 20 min",
-            location: {
-                latitude: 1.558050496260768,
-                longitude: 110.34743759630511,
-            },
             courier: {
                 avatar: images.avatar_4,
                 name: "Muthu"
@@ -291,10 +263,6 @@ const MainCategorias = () => {
             priceRating: affordable,
             photo: images.kek_lapis_shop,
             duration: "35 - 40 min",
-            location: {
-                latitude: 1.5573478487252896,
-                longitude: 110.35568783282145,
-            },
             courier: {
                 avatar: images.avatar_1,
                 name: "Jessie"
@@ -331,7 +299,6 @@ const MainCategorias = () => {
     const [categories, setCategories] = React.useState(categoryData)
     const [selectedCategory, setSelectedCategory] = React.useState(null)
     const [restaurants, setRestaurants] = React.useState(restaurantData)
-    const [currentLocation, setCurrentLocation] = React.useState(initialCurrentLocation)
 
     function onSelectCategory(category) {
         //filter restaurant
@@ -491,7 +458,7 @@ const MainCategorias = () => {
                         source={item.photo}
                         resizeMode="cover"
                         style={{
-                            width: "100%",
+                            width: "90%",
                             height: 200,
                             borderRadius: SIZE.radius
                         }}
